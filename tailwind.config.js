8399ebcebfa1dc4bfa86af0d104e6fbe0e1ccddf
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-19 14:34:39 +0700

    npm dev mode

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["content/**/*.md", "themes/Hugo-Theme/layouts/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
