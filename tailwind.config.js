f6699697706a75c0fd8c4771fdbb6b4a723dc70d
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-19 16:34:19 +0700

    Support for darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+/* NOTE: darkMode here may be removed, but npm run dev will have to change. Ideally, this should be done.
+ *       Instead of running npx from the root, run it from the theme directory. This will allow the theme to
+ *       be standalone.
+ */
 module.exports = {
   content: ["content/**/*.md", "themes/Hugo-Theme/layouts/**/*.html"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
